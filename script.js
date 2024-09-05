d5b5ef841b3325375c1175b350b06f7a0337e49a
Ivan Borisenko <shimy92@mail.ru>
2024-09-05 22:18:38 +0300

    final version without links

--- script.js
+++ script.js
@@ -9,3 +9,45 @@ btns.forEach((btn) => {
     }
   });
 });
+
+const arrow1 = document.querySelector(".arrow1");
+arrow1.addEventListener("click", () => {
+  console.log("click1");
+  addEventListener;
+});
+
+const arrow2 = document.querySelector(".arrow2");
+arrow2.addEventListener("click", () => {
+  console.log("click2");
+});
+const months = [
+  "Январь",
+  "Февраль",
+  "Март",
+  "Апрель",
+  "Май",
+  "Июнь",
+  "Июль",
+  "Август",
+  "Сентябрь",
+  "Октябрь",
+  "Ноябрь",
+  "Декабрь",
+];
+
+let currentMonthIndex = 6;
+
+const month = document.querySelector(".months");
+if (
+  arrow1.addEventListener("click", () => {
+    currentMonthIndex = (currentMonthIndex - 1 + months.length) % months.length;
+    month.textContent = months[currentMonthIndex];
+  })
+);
+
+if (
+  arrow2.addEventListener("click", () => {
+    currentMonthIndex = (currentMonthIndex + 1) % months.length;
+    month.textContent = months[currentMonthIndex];
+  })
+);
